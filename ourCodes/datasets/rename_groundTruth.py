48c8b0a5211a3ef5c0c57290ec67b31a2325a9a0
Emiyali <Emiyalzn@gmail.com>
2021-11-23 11:19:52 +0800

    finish dataset

--- ourCodes/datasets/rename_groundTruth.py
+++ ourCodes/datasets/rename_groundTruth.py
@@ -1,21 +1,29 @@
 import os
 
 def rename(root):
-    sum_list = [0] * 1743
+    sum_list = [0] * 10000
     sum = 0
-    for i in range(1743):
+    for i in range(9880):
         name = f'QR_{i}.jpg'
         file = os.path.join(root, name)
         if not os.path.isfile(file):
             sum += 1
         sum_list[i-sum+1] = sum
 
-    for i in range(1743):
+    for i in range(9880):
         orig_name = f'QR_{i+sum_list[i]}.jpg'
         orig_file = os.path.join(root, orig_name)
         new_name = f'QR_{i}.jpg'
         new_file = os.path.join(root, new_name)
         os.rename(orig_file, new_file)
 
+def addBias(root, bias):
+    for i in range(6624, 7624, 1):
+        orig_name = f'QR_{i}.jpg'
+        new_name = f'QR_{i+bias}.jpg'
+        orig_file = os.path.join(root, orig_name)
+        new_file = os.path.join(root, new_name)
+        os.rename(orig_file, new_file)
+
 if __name__ == '__main__':
-    rename("groundTruth_back")
+    addBias("ToDo/change", -6624)
